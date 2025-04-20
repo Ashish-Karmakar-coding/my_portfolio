f6428eeb66363290e81d845b8b2c3ab53ca678b8
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-04-21 00:21:59 +0530

    Add some animation to the navbar and home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,17 @@
 import "./Home.css";
-import robotImage from "../Images/robot.jpg";
+import { useState, useEffect } from "react";
+import robotImage from "../Images/robot2.jpg";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <>
       <div className="container">
-        <div className="div1">
+        <motion.div className="div1"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
             <div>
           <div className="heading">
             <h1>
@@ -15,16 +21,19 @@ function Home() {
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
             inventore earum culpa sequi nostrum excepturi perspiciatis
-            asperiores ratione aut, ut recusandae laudantium amet fugit suscipit
-            voluptas labore consequuntur quas optio delectus natus quidem quis.
+            asperiores ratione aut
           </p>
           </div>
-        </div>
-        <div className="div2">
+        </motion.div>
+        <motion.div className="div2"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           <div className="img">
-          <img src={robotImage} alt="" />
+            <img src={robotImage} alt="" />
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -10,10 +10,27 @@ function NavBar(){
             <div className="nav-container">
                 <nav className="nav-bar ">
                     <ul className="nav-list">
-                        <li className="nav-item Home" ><a href="#home">Home</a></li>
-                        <li className="nav-item AboutMe "><a href="#about-me">About Me</a></li>
-                        <li className="nav-item Projects"><a href="#projects">Projects</a></li>
-                        <li className="nav-item Contact"><a href="#contact">Contact</a></li>
+                        <li className="nav-item Home"><motion.div
+                            initial={{ opacity: 0, y: -100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6 }}
+                        ><a href="#home">Home</a></motion.div></li>
+                        <li className="nav-item AboutMe "><motion.div
+                            initial={{ opacity: 0, y: -100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8 }}
+                        ><a href="#about-me">About Me</a></motion.div></li>
+                        <li className="nav-item Projects"><motion.div
+                            initial={{ opacity: 0, y: -100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1 }}
+
+                        ><a href="#projects">Projects</a></motion.div></li>
+                        <li className="nav-item Contact"><motion.div
+                            initial={{ opacity: 0, y: -100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1.2 }}
+                        ><a href="#contact">Contact</a></motion.div></li>
                     </ul>
                 </nav>
             </div>
